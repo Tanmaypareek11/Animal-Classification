f60b37f62dd6fc174ba1dd1a83f791dd2047bb5a
Tanmay Pareek <tanmaypareek11@gmail.com>
2025-08-14 14:31:59 +0530

    Add trained model file

--- app.py
+++ app.py
@@ -57,3 +57,13 @@ def predict():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # This enables CORS for all routes
+
+@app.route('/')
+def home():
+    return "CORS is now enabled!"
